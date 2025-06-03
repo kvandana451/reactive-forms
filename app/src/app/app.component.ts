cb6960da86ce0fd393519f20ed2af9657f4f40f5
kvandana451 <kvandana451@gmail.com>
2025-06-03 23:01:27 +0530

    adding a form control

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NameEditorComponent } from './components/name-editor/name-editor.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NameEditorComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'app';
---
+++ app/src/app/components/name-editor/name-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-name-editor',
+  imports: [ReactiveFormsModule],
+  templateUrl: './name-editor.component.html',
+  styleUrl: './name-editor.component.scss',
+})
+export class NameEditorComponent {
+  name = new FormControl('');
+}
