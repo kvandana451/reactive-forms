172f20fe75174a91f27e563d3d339cc8e5aa62f9
kvandana451 <kvandana451@gmail.com>
2025-06-13 19:49:20 +0530

    displaying a form control value

--- app/src/app/components/name-editor/name-editor.component.ts
+++ app/src/app/components/name-editor/name-editor.component.ts
@@ -1,12 +1,22 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-name-editor',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, AsyncPipe],
   templateUrl: './name-editor.component.html',
   styleUrl: './name-editor.component.scss',
 })
 export class NameEditorComponent {
-  name = new FormControl('');
+  name = new FormControl('Vandana');
+  designation = new FormControl('Junior Angular Developer');
+  desg: any = '';
+  country = new FormControl();
+  ngOnInit() {
+    this.designation.valueChanges.subscribe((value: string | null) => {
+      console.log(value);
+      this.desg = value;
+    });
+  }
 }
